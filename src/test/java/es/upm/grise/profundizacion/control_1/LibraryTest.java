97d313d60407ca78ee936c44c8e556a881cfbadf
Pabloabril <p.deandres@alumnos.upm.es>
2022-11-05 12:18:02 +0100

    Nuevos tests creados

--- src/test/java/es/upm/grise/profundizacion/control_1/LibraryTest.java
+++ src/test/java/es/upm/grise/profundizacion/control_1/LibraryTest.java
@@ -1,10 +1,23 @@
 package es.upm.grise.profundizacion.control_1;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 public class LibraryTest {
+
+	@Test
+	public void addTwoBookTest() {
+		Library library = new Library();
+		Book libro1 = new Book("Blancanieves");
+		Book libro2 = new Book("Blancanieves");
+		assertThrows(DuplicatedBookException.class,()->{library.addBook(libro1); library.addBook(libro2);},"Libro duplicado");
+	}
 	
 	@Test
-	public void test() {
-		fail();
+	public void eliminarLibroListaVacia() {
+		Library library = new Library();
+		assertThrows(EmptyLibraryException.class,()->library.getBook("Blancanieves"));
 	}
 
 }
